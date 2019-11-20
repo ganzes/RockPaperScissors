cdabddff719e698185eb2a6132a23354c7a1024a
ganzes <charczuk.arkadiusz@gmail.com>
2019-11-20 23:03:27 +0100

    second commit for fun

--- rps/src/main/java/rpsgame/RpsRunner.java
+++ rps/src/main/java/rpsgame/RpsRunner.java
@@ -2,7 +2,6 @@ package rpsgame;
 
 import java.util.Random;
 import java.util.Scanner;
-import java.util.Timer;
 
 public class RpsRunner {
     public static void main(String[] args) {
@@ -19,28 +18,60 @@ public class RpsRunner {
 
         turtorial.turtorialInfo();
 
-        Random random = new Random();
-        int pcChoice = random.nextInt(3) +1;
-
-
         int i;
+        int playerWin = 0;
+        int pcWin = 0;
         for (i =1 ; i < playersChosenRound; i++){
             System.out.println("Rozpoczecie rundy " + i + "\n Wybierz swoj ruch!");
 
-            int playerRockPaperScissors = playersChoices.nextInt();
+            Random random = new Random();
+            int pcChoice = random.nextInt(3) +1;
 
-            if(playerRockPaperScissors == 1){
-                System.out.println("wybrales kamien");
-            } else if (playerRockPaperScissors == 2){
-                System.out.println("wybrales nozyce");
-            } else if (playerRockPaperScissors == 3){
-                System.out.println("wybrales nozyce");
+            int playerRockPaperScissors = playersChoices.nextInt();
+            int pcRockPaperScissors = pcChoice;
+
+            if(playerRockPaperScissors == 1 && pcRockPaperScissors == 1){
+                System.out.println("wybrales kamien a pc kamien bo wybral " + pcRockPaperScissors + " wiec remis!");
+
+            } else if (playerRockPaperScissors == 1 && pcRockPaperScissors == 2){
+                System.out.println("wybrales kamien a pc papier bo wybral " + pcRockPaperScissors + " wiec przegrales");
+                pcWin++;
+            } else if (playerRockPaperScissors == 1 && pcRockPaperScissors == 3){
+                System.out.println("wybrales kamien a pc nozyce bo wybral " + pcRockPaperScissors + " wiec wygrales");
+                playerWin++;
             }
 
+            if(playerRockPaperScissors == 2 && pcRockPaperScissors == 2){
+                System.out.println("wybrales papier a pc papier bo wybral " + pcRockPaperScissors + " wiec remis!");
+            } else if (playerRockPaperScissors == 2 && pcRockPaperScissors == 1){
+                System.out.println("wybrales papier a pc nozyce bo wybral " + pcRockPaperScissors + " wiec przegrales");
+                pcWin++;
+            } else if (playerRockPaperScissors == 2 && pcRockPaperScissors == 3){
+                System.out.println("wybrales papier a pc kamien bo wybral " + pcRockPaperScissors + " wiec wygrales");
+                playerWin++;
+            }
 
-            System.out.println("Co teraz?");
+            if(playerRockPaperScissors == 3 && pcRockPaperScissors == 3){
+                System.out.println("wybrales nozyce a pc nozyce bo wybral " + pcRockPaperScissors + " wiec remis!");
+            } else if (playerRockPaperScissors == 3 && pcRockPaperScissors == 1){
+                System.out.println("wybrales nozyce a pc kamien bo wybral " + pcRockPaperScissors + " wiec przegrales");
+                pcWin++;
+            } else if (playerRockPaperScissors == 3 && pcRockPaperScissors == 2){
+                System.out.println("wybrales nozyce a pc papier bo wybral " + pcRockPaperScissors + " wiec wygrales");
+                playerWin++;
+            }
+
+            System.out.println("Co teraz? twoja wygrana \n" + playerWin);
 
         }
+
+        System.out.println("Ostateczna wygrana:\n" + "Zdobyłeś " + playerWin + " punktow!" + "\nPC zdobył " + pcWin + " punktow");
+
+        if (playerWin > pcWin){
+            System.out.println("Wygrałes!");
+        } else if (playerWin < pcWin){
+            System.out.println("Przegrales!");
+        } else System.out.println("Remis!");
 
         /*boolean end = false;
         while (!end){
